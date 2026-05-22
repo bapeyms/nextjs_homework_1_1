19e47a4ebc4890bf685c3d21c457a033b3b04836
Katherine <katerynaunik@gmail.com>
2026-05-22 17:12:19 +0300

    part of

---
+++ src/app/(components)/homePage.tsx
@@ -0,0 +1,21 @@
+type HomePageProps = {
+    column1: String;
+    column2: String;
+    column3: String;
+}
+
+export default function HomePageView({column1, column2, column3}: HomePageProps) {
+    return <>
+    <div className="grid grid-cols-3">
+        <div>
+            <h1>{column1}</h1>
+        </div>
+        <div>
+            <h1>{column2}</h1>
+        </div>
+        <div>
+            <p>{column3}</p>
+        </div>
+    </div>
+    </>
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
 import Image from "next/image";
-import HomePage from "./homePage/page";
 import Link from "next/link";
+import HomePageView from "./(components)/homePage";
 
 export default function Home() {
+  const pageData = {
+    first: "Welcome Home (Column 1)",
+    second: "Middle Content (Column 2)",
+    third: "Right Content (Column 3)"
+  };
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <HomePage></HomePage>
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black min-h-screen">
+      <HomePageView 
+        column1={pageData.first}
+        column2={pageData.second}
+        column3={pageData.third}
+      />
     </div>
   );
 }
